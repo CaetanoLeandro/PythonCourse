dece8d5af50cd695efec8a8a5509e3d9d382c39d
Caetano <lcaetano30@gmail.com>
2022-01-20 23:06:51 -0300

    jogo de adivinhação

--- adivinhacao.py
+++ adivinhacao.py
@@ -1,1 +1,17 @@
-print("Hello word")
+
+print("#####################################")
+print("Bem vindo ao jogo de adivinhação")
+print("#####################################")
+
+numero_secreto = 42
+
+chute = input("Digite o seu número: ")
+
+print("Voce digitou: ", chute)
+
+chute = int(chute) #converte string >> inteiro
+
+if(numero_secreto == chute):
+    print("Você acertou")
+else:
+    print("Você errou")
